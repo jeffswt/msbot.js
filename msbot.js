09e1fb26cc690d832cc8389672ccaafa0faaed5e
Geoffrey, Tang <jeffswt@outlook.com>
2017-04-17 13:05:44 +0800

    Implemented containment in 2d queue.

--- msbot.js
+++ msbot.js
@@ -2,5 +2,11 @@ var Queue2D = function() {
     this.length = 0;
     this.arr_x = [];
     this.arr_y = [];
+    this.includes = function(x, y) {
+        for (var i = 0; i < this.length; i++)
+            if (x == this.arr_x[i] && y == this.arr_y[i])
+                return true;
+        return false;
+    }
     return this;
 };
